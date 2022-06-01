ae76d9901cdae1501cf082fc89d0e473c61ed215
김 수정 <kim@gim-ui-iMac.local>
2022-06-01 22:06:40 +0900

    css_grid_prac

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <div className='nemo1'></div>
+        <div className='nemo2'></div>
+        <div className='nemo3'></div>
+        <div className='nemo4'></div>
+        <div className='nemo5'></div>
+        <div className='nemo6'></div>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 100px);
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: 100px;
+  column-gap: 5px;
+  row-gap: 5px;
+  width: 400px;
+  height: auto;
+  padding: 5px;
+  border-radius: 20px;
+  border: 5px solid #222;
+  .nemo1{
+    background: red;
+    border: 5px solid #222;
+    border-radius: 20px;
+    grid-column: 1/4
+  }
+  .nemo2{
+    background: orange;
+    border: 5px solid #222;
+    border-radius: 20px;
+  }
+  .nemo3{
+    background: yellow;
+    border: 5px solid #222;
+    border-radius: 20px;
+    grid-column: span 2;
+  }
+  .nemo4{
+    background: green;
+    border: 5px solid #222;
+    border-radius: 20px;
+  }
+  .nemo5{
+    background: blue;
+    border: 5px solid #222;
+    border-radius: 20px;
+  }
+  .nemo6{
+    background: purple;
+    border: 5px solid #222;
+    border-radius: 20px;
+  }
+`
 
 export default App;
